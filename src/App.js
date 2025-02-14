7faf2523d50e4235f42cfd5876844cea29ab0334
Dwijasri <157499031+Dwijasri@users.noreply.github.com>
2025-02-14 11:42:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,98 @@
-import React, { createContext, useContext } from "react";
-const UserContext = createContext();
-const ProfileWithDrilling = ({ user }) => {
-  return <ProfileDetails user={user} />;
-};
+import React, {useState} from 'react';
+import Child from './ChildA.js'
 
-const ProfileDetails = ({ user }) => {
-  return (
-    <div>
-      <h2>Profile (Prop Drilling)</h2>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-    </div>
-  );
-};
-const ProfileWithContext = () => {
-  const user = useContext(UserContext);
-  return (
-    <div>
-      <h2>Profile (React Context)</h2>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-    </div>
-  );
-};
+const Parent=()=>{
+  const[message,setMessage]=useState('Hello World')
 
-// App Component
-const App = () => {
-  const user = { name: "Alice", email: "Alice@123" };
+  const changeMessage=()=>{
+    setMessage("Clicked the button");
+  };
 
-  return (
+  return(
     <div>
-      <h1>User Profile App</h1>
-      {/* Prop Drilling Example */}
-      <ProfileWithDrilling user={user} />
-      {/* React Context Example */}
-      <UserContext.Provider value={user}>
-        <ProfileWithContext />
-      </UserContext.Provider>
+      <h1>{message}</h1>
+      <Child changeMessage={changeMessage}/>
     </div>
-  );
-};
-export default App;
+  )
+}
+
+export default Parent;
+
+// import React,{useReducer} from "react";
+
+// function appleReducer(state={count:0, apples:[]},action){
+//   switch(action.type){
+//     case 'ADD_APPLE':
+//       return {count: state.count+1, apples: [...state.apples, 'apple']};
+//     case 'EAT_APPLE':
+//       if(state.count > 0){
+//         return {count: state.count-1, apples: state.apples.slice(0, -1)};
+//       }
+//       return state;
+//     default:
+//       return state;
+//   }
+// }
+// function App() {
+//   const [state,dispatch]=useReducer(appleReducer,{count:0, apples:[]})
+//   return (
+//     <div>
+//       <h1>Chocolate counter</h1>
+//       <p>Number of Chocolates:{state.count}</p>
+//       <div>
+//         {state.apples.map((apple, index) => (
+//           <img key={index} src="https://tse4.mm.bing.net/th?id=OIP.vY9xacRUMHNEXlagqOM9rQHaFH&pid=Api&P=0&h=180"  />
+//         ))}
+//       </div>
+//       <button onClick={()=>dispatch({type:'ADD_APPLE'})}>Add Chocolate</button>
+//       <button onClick={()=>dispatch({type:'EAT_APPLE'})}>Eat Chocolate</button>
+//     </div>
+//   )
+// }
+// export default App;
+// import React, { createContext, useContext } from "react";
+// const UserContext = createContext();
+// const ProfileWithDrilling = ({ user }) => {
+//   return <ProfileDetails user={user} />;
+// };
+
+// const ProfileDetails = ({ user }) => {
+//   return (
+//     <div>
+//       <h2>Profile (Prop Drilling)</h2>
+//       <p>Name: {user.name}</p>
+//       <p>Email: {user.email}</p>
+//     </div>
+//   );
+// };
+// const ProfileWithContext = () => {
+//   const user = useContext(UserContext);
+//   return (
+//     <div>
+//       <h2>Profile (React Context)</h2>
+//       <p>Name: {user.name}</p>
+//       <p>Email: {user.email}</p>
+//     </div>
+//   );
+// };
+
+// // App Component
+// const App = () => {
+//   const user = { name: "Alice", email: "Alice@123" };
+
+//   return (
+//     <div>
+//       <h1>User Profile App</h1>
+//       {/* Prop Drilling Example */}
+//       <ProfileWithDrilling user={user} />
+//       {/* React Context Example */}
+//       <UserContext.Provider value={user}>
+//         <ProfileWithContext />
+//       </UserContext.Provider>
+//     </div>
+//   );
+// };
+// export default App;
 
 
 
--- src/ChildA.js
+++ src/ChildA.js
@@ -1,12 +1,20 @@
+import React from 'react';
+const Child=({changeMessage})=>{
+    return(
+        <button onClick={changeMessage}>Click me</button>
+    )
+}
+export default Child;
+
 // const ChildA=()=>{
 //     console.log("Child A rendered")
 //     return <h2> This is Child A</h2>
 // };
 // export default ChildA;
 
-import {memo} from 'react';
-const ChildA=()=>{
-    console.log("Child A rendered")
-    return <h2>This is Child A</h2>
-};
-export default memo(ChildA);
+// import {memo} from 'react';
+// const ChildA=()=>{
+//     console.log("Child A rendered")
+//     return <h2>This is Child A</h2>
+// };
+// export default memo(ChildA);
